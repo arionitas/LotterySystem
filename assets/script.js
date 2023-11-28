acc20644580308dfd3d17622eebef391d42b85ff
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-11-28 20:31:07 +0000

    added simple pop up to appear on screen when 'Enter Lottery' button is clicked.

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,11 @@
-// User class to represent users
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
 
-class User {
-    constructor(name, email) {
-        this.name = name;
-        this.email = email;
-    }
-}
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+});
 
-// Lottery class to manage lotteries
-
-class Lottery {
-    constructor(availableTickets, entryDeadline) {
-        this.availableTickets = availableTickets;
-        this.entryDeadline = entryDeadline;
-        this.entries = [];
-    }
-    enter(user) {
-        this.entries.push(user);
-    }
-    drawWinners() {
-        return this.entries.slice(0, this.availableTickets);
-    }
-}
-
-// LotterySystem class to manage multiple lotteries
-
-class LotterySystem {
-    constructor() {
-        this.lotteries = [];
-    }
-    createLottery(availableTickets, entryDeadline) {
-        const lottery = new Lottery(availableTickets, entryDeadline);
-        this.lotteries.push(lottery);
-        return lottery;
-    }
-}
-
-// Initialize the lottery system
-
-const lotterySystem = new LotterySystem();
-
-// Example user
-
-const user = new User("Arion Mehmeti", "mgmt@arxivn.com");
-
-// Create a lottery
-
-const showLottery = lotterySystem.createLottery(10, "2023-12-01 T-18:00:00");
-
-// Display lottery info
-
-document.getElementById("lottery-info").innerHTML = `Enter the lottery for a chance to win tickets! Deadline: ${showLottery.entryDeadline}`;
-
-// Handle user entering the lottery
-
-function enterLottery() {
-    showLottery.enter(user);
-    alert("Lottery entered successfully!");
-}
-
-let popup = document.getElementById("popup")
-
-function openPopup() {}
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+});
