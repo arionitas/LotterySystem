f40403ad818530b51a201490538bbc2ced2d855a
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-11-28 16:15:41 +0000

    added images with hover effects alongisde a logo for TodayTix. Implemented an alert system telling the user they have successfully been included in the lottery system

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,109 @@
-class user {
+// User class to represent users
+
+class User {
+
     constructor(name, email) {
+
         this.name = name;
+
         this.email = email;
+
     }
+
 }
+
+
+
+// Lottery class to manage lotteries
 
 class Lottery {
+
     constructor(availableTickets, entryDeadline) {
+
         this.availableTickets = availableTickets;
+
         this.entryDeadline = entryDeadline;
+
         this.entries = [];
+
     }
+
+
 
     enter(user) {
+
         this.entries.push(user);
+
     }
+
+
 
     drawWinners() {
+
         return this.entries.slice(0, this.availableTickets);
+
     }
+
 }
+
+
+
+// LotterySystem class to manage multiple lotteries
 
 class LotterySystem {
+
     constructor() {
+
         this.lotteries = [];
+
     }
+
+
 
     createLottery(availableTickets, entryDeadline) {
-        const lottery = new
-        Lottery(availableTickets, entryDeadline);
+
+        const lottery = new Lottery(availableTickets, entryDeadline);
+
         this.lotteries.push(lottery);
+
         return lottery;
+
     }
+
 }
 
+
+
+// Initialize the lottery system
+
 const lotterySystem = new LotterySystem();
-const user = new User("Arion Mehmeti", "mgmt@arxivn.com");
 
-// Creating a lottery below
-const showLottery = lotterySystem.createLottery(10, "2023-11-28T21:00:00");
 
-// Showing the lottery information
-document.getElementById("lottery-information").innerHTML = `Enter the TodayTix Lottery for a chance to win tickets! Deadline: $ {showLottery.entryDeadline}`;
 
-// User will enter the lottery at this stage
+// Example user
+
+const user = new User("John Doe", "john@example.com");
+
+
+
+// Create a lottery
+
+const showLottery = lotterySystem.createLottery(10, "2023-12-01T18:00:00");
+
+
+
+// Display lottery info
+
+document.getElementById("lottery-info").innerHTML = `Enter the lottery for a chance to win tickets! Deadline: ${showLottery.entryDeadline}`;
+
+
+
+// Handle user entering the lottery
+
 function enterLottery() {
+
     showLottery.enter(user);
-    alert("Lottery Entery Successful!");
+
+    alert("Lottery entered successfully!");
+
 }
