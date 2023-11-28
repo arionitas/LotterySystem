d31304472c4adc2eebfa47b76efc6e025e918a6a
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-11-28 17:32:11 +0000

    added hover effects to imagery and a navbar with logo of TodayTix

--- assets/script.js
+++ assets/script.js
@@ -1,109 +1,60 @@
 // User class to represent users
 
 class User {
-
     constructor(name, email) {
-
         this.name = name;
-
         this.email = email;
-
     }
-
 }
-
-
 
 // Lottery class to manage lotteries
 
 class Lottery {
-
     constructor(availableTickets, entryDeadline) {
-
         this.availableTickets = availableTickets;
-
         this.entryDeadline = entryDeadline;
-
         this.entries = [];
-
     }
-
-
-
     enter(user) {
-
         this.entries.push(user);
-
     }
-
-
-
     drawWinners() {
-
         return this.entries.slice(0, this.availableTickets);
-
     }
-
 }
-
-
 
 // LotterySystem class to manage multiple lotteries
 
 class LotterySystem {
-
     constructor() {
-
         this.lotteries = [];
-
     }
-
-
-
     createLottery(availableTickets, entryDeadline) {
-
         const lottery = new Lottery(availableTickets, entryDeadline);
-
         this.lotteries.push(lottery);
-
         return lottery;
-
     }
-
 }
-
-
 
 // Initialize the lottery system
 
 const lotterySystem = new LotterySystem();
 
-
-
 // Example user
 
-const user = new User("John Doe", "john@example.com");
-
-
+const user = new User("Arion Mehmeti", "mgmt@arxivn.com");
 
 // Create a lottery
 
-const showLottery = lotterySystem.createLottery(10, "2023-12-01T18:00:00");
-
-
+const showLottery = lotterySystem.createLottery(10, "2023-12-01 T-18:00:00");
 
 // Display lottery info
 
 document.getElementById("lottery-info").innerHTML = `Enter the lottery for a chance to win tickets! Deadline: ${showLottery.entryDeadline}`;
 
-
-
 // Handle user entering the lottery
 
 function enterLottery() {
-
     showLottery.enter(user);
-
     alert("Lottery entered successfully!");
-
 }
