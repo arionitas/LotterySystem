7aa8034c54dd239aafedd44a2849b1a4576df082
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-12-01 12:56:49 +0000

    adding some extra button functions

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,26 @@
 const openBtn = document.getElementById("openModal");
+const openBtn2 = document.getElementById("openModal2");
+const openBtn3 = document.getElementById("openModal3");
 const closeBtn = document.getElementById("closeModal");
+const closeBtn2 = document.getElementById("closeModal2");
+const closeBtn3 = document.getElementById("closeModal3");
 const modal = document.getElementById("modal");
+const modal2 = document.getElementById("modal2");
 
 openBtn.addEventListener("click", () => {
     modal.classList.add("open");
 });
 
-closeBtn.addEventListener("click", () => {
-    modal.classList.remove("open");
+openBtn2.addEventListener("click", () => {
+    modal2.classList.add("open");
 });
+
+openBtn3.addEventListener("click", () => {
+    modal2.classList.add("open");
+});
+
+
+closeBtn2.addEventListener("click", () => {
+    modal2.classList.remove("open");
+});
+
